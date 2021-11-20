1a5fe4c7c479864f01f394ecb85353438a972f7b
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-20 04:26:48 -0800

    ProductTag.js model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,9 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
+    // // link to database connection
     sequelize,
     timestamps: false,
     freezeTableName: true,
