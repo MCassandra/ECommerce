d2e8d81c4f8105ab19065df24393d257799917ce
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-20 04:03:34 -0800

    Category model

--- models/Category.js
+++ models/Category.js
@@ -6,10 +6,21 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
+    // link to database connection
     sequelize,
+    // set to false to remove "created_at" and "updated_at" fields
     timestamps: false,
     freezeTableName: true,
     underscored: true,
